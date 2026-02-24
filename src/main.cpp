93eff6574c9e01f63e3d41021000c4331348ab41
Ojas Deshpande <ojas.md2006@gmail.com>
2026-02-24 14:36:22 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,9 @@
 
 int main(int argc, char *argv[])
 {
-    // Flush after every std::cout / std::cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     std::cerr << "Logs from your program will appear here" << std::endl;
 
     if (argc != 3)
@@ -29,15 +27,19 @@ int main(int argc, char *argv[])
             return 1;
         }
 
-        // TODO: Uncomment the code below to pass the first stage
-        database_file.seekg(16); // Skip the first 16 bytes of the header
+        database_file.seekg(16);
 
         char buffer[2];
         database_file.read(buffer, 2);
 
         unsigned short page_size = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
 
+        database_file.seekg(103);
+        database_file.read(buffer, 2);
+        unsigned short tables = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
+
         std::cout << "database page size: " << page_size << std::endl;
+        std::cout << "number of tables: " << tables << std::endl;
     }
 
     return 0;
